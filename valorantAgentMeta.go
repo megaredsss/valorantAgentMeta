b4996357797eb00c1438029f6bd5781243bc59ce
megaredsss <58664975+megaredsss@users.noreply.github.com>
2024-09-06 03:10:02 +0300

    last version before delete comments

--- valorantAgentMeta.go
+++ valorantAgentMeta.go
@@ -3,18 +3,41 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
+type Pair struct {
+	Key   string
+	Value int
+}
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func sortMapByValues(m map[string]int) PairList {
+	p := make(PairList, len(m))
+
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+		i++
+	}
+
+	sort.Sort(sort.Reverse(p))
+	return p
+}
 func main() {
 
-	var agentsSlice []string
-	var mapsSlice []string
-	var data map[string]map[string]int
+	type Row map[string]string
 	// var allMaps []map[string]map[string]int
+	type Table []Row
+	// tables := []Table{}
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
@@ -25,52 +48,121 @@ func main() {
 	})
 
 	c.OnHTML(".wf-card.mod-dark.mod-table.mod-scroll table.wf-table.mod-pr-global", func(e *colly.HTMLElement) {
+		table := Table{}
+		var headers []string
 		e.ForEach("tr", func(i int, tr *colly.HTMLElement) {
+			row := Row{}
+			//fmt.Println(i)
 			// if i > 1 {
 			// fmt.Println("0")
 			// currentMap := make(map[string]map[string]int)
-
-			tr.ForEach("th img", func(i int, th *colly.HTMLElement) {
-				re := regexp.MustCompile("/img/vlr/game/agents/(.*).png")
-				agentsSlice = append(agentsSlice, re.ReplaceAllString(th.Attr("src"), "${1}"))
-				// fmt.Println(agentsSlice)
-			})
-			if i > 1 {
-				tds := tr.DOM.Find("td")
-				tds.Find("span").Remove()
-				if tds.Length() > 0 {
-					firstColumnText := tds.First().Text()
-					firstColumnText = strings.TrimSpace(firstColumnText)
-					mapsSlice = append(mapsSlice, firstColumnText)
+			if i == 0 {
+				tr.ForEach("th", func(i int, h *colly.HTMLElement) {
+					if i == 0 {
+						headers = append(headers, h.Text)
+						//fmt.Println(h.Text)
+					}
+				})
+				tr.ForEach("th img", func(i int, th *colly.HTMLElement) {
+					re := regexp.MustCompile("/img/vlr/game/agents/(.*).png")
+					headers = append(headers, strings.TrimSpace(re.ReplaceAllString(th.Attr("src"), "${1}")))
+					//fmt.Println(headers)
+				})
+			} else if i > 1 {
+				//fmt.Println(i)
+				tr.ForEach("td", func(i int, td *colly.HTMLElement) {
+					//fmt.Println("td = ", td)
+					//fmt.Println("td.Class = ", td.Attr("class"))
+					//fmt.Println("td.Attr = ", td.Attr("style"))
+					if td.Attr("style") == "white-space: nowrap; padding-top: 0; padding-bottom: 0;" {
+						td.DOM.Find("span").Remove()
+						if i < len(headers) {
+							header := headers[i]
+							row[header] = strings.TrimSpace(strings.TrimSpace(td.DOM.Text()))
+							//fmt.Println(row[header])
+						}
+					}
+					// if td.Attr("class") == "mod-right" {
+					// 	//fmt.Println("td.Text = ", strings.TrimSpace(tds))
+					// 	if i < len(headers) {
+					// 		header := headers[i]
+					// 		row[header] = strings.TrimSpace(td.Text)
+					// 	}
+					//fmt.Println(td.DOM.Text())
+					// tds := tr.DOM.Find("td")
+					// tds.Find("span").Remove()
+					// //fmt.Println(tds.Text())
+					// if i < len(headers) {
+					// 	header := headers[i]
+					// 	row[header] = strings.TrimSpace(tds.Text())
+					// }
+					//}
+					if td.Attr("class") == "mod-color-sq" {
+						//fmt.Println(td.ChildText("span"))
+						//fmt.Println(i, " ", len(headers))
+						//if i < len(headers) {
+						//fmt.Println(i)
+						header := headers[i-3]
+						//fmt.Println(headers[i-3])
+						//fmt.Println(headers[i-3])
+						row[header] = strings.TrimSpace(td.ChildText("span"))
+						//fmt.Println(strings.TrimSpace(td.ChildText("span")))
+						// if i < len(heajders) {
+						// 	header := headers[i]
+						// 	row[header] = strings.TrimSpace(td.Text)
+						// }
+						//}
+					}
+					//fmt.Println(table)
+				})
+				if len(row) > 0 {
+					table = append(table, row)
+					//fmt.Println(table)
 				}
 			}
 		})
-		// tr.ForEach("td", func(i int, td *colly.HTMLElement){
-		// 				if i == 0{
-		// 					currentMap[
-		// 				}
+		//fmt.Println(table)
+		// if len(table) > 0 {
+		// 	tables = append(tables, table)
 		// }
-
-		fmt.Println(mapsSlice)
-		fmt.Println(agentsSlice)
-		e.ForEach("tr", func(i int, tr *colly.HTMLElement) {
-			tr.ForEach("td", func(j int, td *colly.HTMLElement) {
-				// html, _ := td.DOM.Html()
-				text := td.DOM.Find("tr.pr-global-row:nth-child(3) > td:nth-child(5) > div:nth-child(1)").Text()
-				text = strings.TrimSpace(text)
-				fmt.Println(text)
-				// fmt.Printf("HTML ячейки %d в строке %d: %s\n", j, i, html)
-				if i > 1 && j > 3 {
-					data = make(map[string]map[string]int)
-					data[mapsSlice[i-1]] = make(map[string]int)
-					selection := td.DOM.Find("div.color-sq > span")
-					fmt.Println(selection.Html())
-					data[mapsSlice[i-2]][agentsSlice[j-4]], _ = strconv.Atoi(selection.Text())
-					fmt.Println(data)
+		for i := range table {
+			for key, value := range table[i] {
+				newValue := strings.ReplaceAll(value, "%", "")
+				table[i][key] = newValue
+			}
+		}
+		var saveMap map[string]string
+		var testValMap string
+		fmt.Scanln(&testValMap)
+		copyNext := false
+		mapWithIntValue := make(map[string]int)
+		for i := range table {
+			for key, value := range table[i] {
+				if key == "Map" && value == testValMap {
+					copyNext = true
 				}
-			})
-		})
-		fmt.Println(data)
+				if copyNext {
+					delete(table[i], "Map")
+					//fmt.Println(table[i])
+					saveMap = table[i]
+					copyNext = false
+				}
+			}
+
+		}
+		//fmt.Println(saveMap)
+		for key, value := range saveMap {
+			mapWithIntValue[key], _ = strconv.Atoi(value)
+		}
+		//fmt.Println(mapWithIntValue)
+		result := sortMapByValues(mapWithIntValue)
+		//fmt.Println(result)
+		// for i := range result {
+		// 	fmt.Println(i)
+		// }
+		for i := 0; i < 5; i++ {
+			fmt.Print(result[i].Key, " ")
+		}
 	})
 
 	fmt.Println("Scrapping Completed")
